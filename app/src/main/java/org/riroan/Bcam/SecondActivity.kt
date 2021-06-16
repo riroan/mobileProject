d43874d88744aa4740efc836f975d2c5cf3608f5
lminjung99 <lminjung999@gmail.com>
2021-06-16 11:58:00 +0900

    필터메뉴 추가, 촬영 후 화면 수정(미완성)

--- app/src/main/java/org/riroan/Bcam/SecondActivity.kt
+++ app/src/main/java/org/riroan/Bcam/SecondActivity.kt
@@ -1,11 +1,27 @@
 package org.riroan.Bcam
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+import org.riroan.Bcam.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySecondBinding
+    var imgPath:String?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        if(intent.hasExtra("imagePath")){
+            imgPath = intent.getStringExtra("imagePath")
+        }
+        var bitmap = BitmapFactory.decodeFile(imgPath)
+        imageView.setImageBitmap(bitmap)
     }
 }
